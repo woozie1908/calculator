26882aaa80fb69a3193da47173adf7b31a89b00e
woozie1908 <erwinjung1995@gmail.com>
2026-02-06 21:41:01 -0800

    Math functions log in console

--- calculator.js
+++ calculator.js
@@ -2,14 +2,21 @@
 var add = function (a, b) {
     return a + b;
 };
-var resultAdd = add(8, 8);
+//Subtract function
+var subtract = function (a, b) {
+    return a - b;
+};
+//Multiply function
+var multiply = function (a, b) {
+    return a * b;
+};
+//Divide function
+var divide = function (a, b) {
+    return a / b;
+};
+var resultAdd = divide(8, 8);
 console.log(resultAdd);
-// the following items and testing them in your browser’s console:
-// add
-// subtract
-// multiply
-// divide
-//Answer Box
+//Answer Box -> Playing around 
 var answerBox = document.createElement('div');
 answerBox.classList.add('box');
 answerBox.placeholder = 'Enter answer here';
@@ -21,26 +28,27 @@ answerBox.style.position = 'absolute';
 answerBox.style.left = '50%';
 answerBox.style.top = '7%';
 answerBox.style.transform = 'translate(-50%, -50%)';
-//Establish Input Response
-var input1 = document.createElement("input");
-input1.type = "number";
-input1.placeholder = "First number";
-var input2 = document.createElement("input");
-input2.type = "number";
-input2.placeholder = "Second number";
-var button = document.createElement("button");
-button.textContent = "Add";
-var result = document.createElement("input");
-result.type = "text";
-result.placeholder = "Result";
-result.readOnly = true;
-button.addEventListener("click", function () {
-    var a = Number(input1.value);
-    var b = Number(input2.value);
-    result.value = add(a, b);
-});
 document.body.appendChild(answerBox);
-answerBox.appendChild(input1);
-answerBox.appendChild(input2);
-answerBox.appendChild(button);
-answerBox.appendChild(result);
+// //Establish Input Response
+// const input1 = document.createElement("input");
+// input1.type = "number";
+// input1.placeholder = "First number";
+// const input2 = document.createElement("input");
+// input2.type = "number";
+// input2.placeholder = "Second number";
+// const button = document.createElement("button");
+// button.textContent = "Add";
+// const result = document.createElement("input");
+// result.type = "text";
+// result.placeholder = "Result";
+// result.readOnly = true;
+// button.addEventListener("click", () => {
+//     const a = Number(input1.value);
+//     const b = Number(input2.value);
+//     result.value = add(a, b);
+//   });
+// document.body.appendChild(answerBox);
+// answerBox.appendChild(input1);
+// answerBox.appendChild(input2);
+// answerBox.appendChild(button);
+// answerBox.appendChild(result);
